1a5e647fe372412feec7aa325ad95b1b2081912e
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2025-09-11 23:48:17 -0300

    feat: template studies

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("UMA MENSAGEM PARA ALGUÉM MUITO FODA, DIRETAMENTO DO VIEWS DO APP HOME!")
+    return render(request,'home/index.html')
 
 
 def home2(request):
