730179bc0950553ff2b18a71d92e5deaf0ea0d13
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2025-12-16 19:47:07 -0300

    feat: adds carPhoto and brands model

--- cars/admin.py
+++ cars/admin.py
@@ -3,7 +3,7 @@ from cars.models import Car, Brand
 
 
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model','factory_year','brand','model_year','value')
+    list_display = ('model','factory_year','brand','model_year','value','plate')
     search_fields = ('model',)
 
 class BrandAdmin(admin.ModelAdmin):
--- cars/models.py
+++ cars/models.py
@@ -14,10 +14,13 @@ class Car(models.Model):
 
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
-    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_name')
+    #                                   v   impede que a tentativa de deleção da brand apague todos os carros, logo para deletar a brand precisa deletar todos os carros.
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
     value= models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
     
     # subscrevendo a função padrão, de criar retornar object 1,2.... para o nome do modelo mesmo.
     # Tipo toString() do java
