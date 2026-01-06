eab20de2ab8d1c4b8645c52250907abc996da2a0
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-01-06 02:48:41 -0300

    feat: vacation changes

--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from cars.models import Car
+
 
 def cars_view(request):
-    return render(request, 'cars.html')
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+    
+    if search:     
+        # e uma queryset, nao apenas uma 
+        cars = Car.objects.filter(model__contains = search)
+
+    return render(
+        request,
+        'cars.html',
+        {'cars': cars }
+        )
