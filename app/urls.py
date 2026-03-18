6c7fbe73ebbab355211c92352ac2fffd57ce6b8a
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-03-18 15:53:42 -0300

    feat: view to detail car

--- app/urls.py
+++ app/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewCarView
+from cars.views import ListCarsView, CreateNewCarView, CarDetailView
 from accounts.views import register_view
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("/register", register_view, name="register"),
-    path('cars/', CarsView.as_view() , name='cars_list'),
-    path('new_car/', NewCarView.as_view() , name="new_car")
+    path('register/', register_view, name='register'),
+    path('cars/', CarView.as_view() , name='cars_list'),
+    path('new_car/', CreateNewCarView.as_view() , name='new_car'),
+    path('car/<id:pk>/', CarDetailView.as_view(), name= 'car_detail')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # import para terminar a configuração de fotos. Obs precisa do uso da lib Pillow
