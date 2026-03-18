a26bd0d807aef62cb5f32ea2b84aaa83bbcbb332
pedroirineu5 <pedrohenriqueirineu@gmail.com>
2026-03-18 15:52:28 -0300

    refact: use of class based views to refactor the legacy class based views

--- cars/views.py
+++ cars/views.py
@@ -2,41 +2,33 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarForm
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView, DetailView
 
-class CarsView(View):
-
-    def get(self, request):
-        
-        cars = Car.objects.all().order_by('model')
-        # essa busca que acontece em uma URL usando 
-        # http://localhost:8000/cars/?search=Teste
-        search = request.GET.get('search')
-    
-        if search:     
-         # e uma queryset, nao apenas uma lista comum
-             cars = Car.objects.filter(model__icontains = search).order_by('model')
-
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars }
-            )
 
 class ListCarsView(ListView):
+    
     model = Car
     template_name = 'cars.html'
     context_object_name = 'cars'
 
-class NewCarView(View):
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        
+        if search:
+            cars = cars.filter(model_icontains=search)
+        
+        return cars
 
-    def get(self, request):
-        new_car_form = CarForm()
-        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+class CreateNewCarView(CreateView):
     
-    def post(self, request):   
-        new_car_form = CarForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form' : new_car_form})
+    model = Car
+    form_class = CarForm
+    template_name = 'new_car.html'
+    success_url = '/car_list/'
+
+
+class CarDetailView(DetailView):
+
+    model = Car
+    template_name = 'car_detail.html'
